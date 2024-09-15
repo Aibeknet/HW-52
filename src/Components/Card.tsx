24b3cc72539e8a9455e53175840d1f8f2c49ccf1
aibeknet <aaskarov0690.kg@gmail.com>
2024-09-15 18:00:41 +0600

    Сreated a component called Card.Level 1 is completed.

---
+++ src/Components/Card.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+interface ICardProps {
+    rank: string;
+    suit: string;
+}
+
+
+
+const Card: React.FC<ICardProps> = ({ rank, suit}) => {
+    const suitsObj: { [key: string]: string } = {
+        diams: '♦',
+        hearts: '♥',
+        clubs: '♣',
+        spades: '♠'
+    };
+
+    const suitSymbol = suitsObj[suit];
+
+    return (
+        <span className={`card rank-${rank} ${suit}`}>
+            <span className="rank">{rank}</span>
+            <span className="suit">{suitSymbol}</span>
+        </span>
+    );
+};
+
+export default Card;
